c6730d85346f2a70e289e5095991dd2bc1f058ab
Grant West <grantwest62@yahoo.com>
2016-09-30 12:49:21 -0700

    Added verticality to level 2 + created levitation zone script
    Provided layout for vertical movement around level 2, as well as hidden
    caches. Also created a levitation zone script that once the player
    enters, allows them to float- can be used to get back to high place
    after falling to secret area

---
+++ System Dot v.06/Assets/Scripts/LevitationZone.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevitationZone : MonoBehaviour {
+
+    public float flightSpeed = 1.1f;
+    public PlayerController player;
+    private bool flying;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+        flying = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(flying)
+        {
+            player.transform.position  = new Vector2(transform.position.x, transform.position.y - (flightSpeed * Time.deltaTime));
+        }
+	}
+
+    void onTriggerEnter2d(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            flying = true;
+        }
+    }
+
+    void onTriggerExit2d(Collider2D other)
+    {
+        flying = false:
+    }
+}
