6998e83f80b08c32ccb8ecbdf0c7aae84fd67ece
Grant West <grantwest62@yahoo.com>
2016-10-05 00:07:56 -0700

    Open Market Script
    Created script to enter market from cpu

---
+++ System Dot v.06/Assets/Scripts/OpenMarket.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenMarket : MonoBehaviour {
+
+
+    public GameObject marketCanvas;
+
+    private bool openMarket;
+
+    private bool inMarket;
+
+	// Use this for initialization
+	void Start () {
+        openMarket = false;
+        inMarket = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W) && inMarket)
+        {
+                openMarket = true;
+        }
+
+        if(openMarket)
+        {
+            marketCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && openMarket)
+        {
+            openMarket = false;
+            marketCanvas.SetActive(true);
+            Time.timeScale = 1f;
+        }
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            inMarket = true;
+        }
+    }
+
+    void OnTriggerExit2d()
+    {
+        
+            inMarket = false;
+        
+    }
+
+
+}
