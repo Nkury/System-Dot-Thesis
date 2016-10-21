29f9dcf3da065fe8a67fbddc145578830c7c2c56
Grant West <grantwest62@yahoo.com>
2016-10-21 03:21:07 -0700

    UI Update
    Added bit and new health display. Set up for tool tip, but need to fixt
    the event system. Need to fix health bar size change

---
+++ System Dot v.06/Assets/Scripts/BitDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BitDisplay : MonoBehaviour {
+
+    Text txt;
+    
+	// Use this for initialization
+	void Start () {
+        txt = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        txt.text = System.Convert.ToString(PlayerStats.bitsCollected ,2);
+	}
+}
---
+++ System Dot v.06/Assets/Scripts/BitToolTip.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BitToolTip : MonoBehaviour {
+
+    public Text txt;
+	// Use this for initialization
+	void Start () {
+        txt.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseEnter()
+    {
+        txt.text = "HELLO";
+    }
+
+    void OnMouseExit()
+    {
+        txt.text = "";
+    }
+}
