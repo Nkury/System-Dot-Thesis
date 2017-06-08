9b2520b484dc617395dfc700317a3a9d6be3aba8
Nizar Kury <nkury@asu.edu>
2017-06-07 22:41:05 -0700

    Created Level Manager system for future levels

---
+++ System Dot v.06/Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelHandler : MonoBehaviour
+{
+
+    public GameObject currentCheckpoint;
+
+    protected PlayerController player;
+
+    public GameObject deathParticle;
+    public GameObject respawnParticle;
+
+    public int pointsPenaltyOnDeath;
+
+    public float respawnDelay;
+
+    protected CameraController camera;
+
+    protected float gravityStore;
+
+    public HealthManager healthManager;
+
+    public static bool canPressTab = true;
+    
+    protected bool loadedIn = false;
+
+    // Use this for initialization
+    public void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+
+        camera = FindObjectOfType<CameraController>();
+
+        healthManager = FindObjectOfType<HealthManager>();
+        LoadLevel();
+        player.transform.position = currentCheckpoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RespawnPlayer()
+    {
+        StartCoroutine("RespawnPlayerCo");
+    }
+
+    public IEnumerator RespawnPlayerCo()
+    {
+        Instantiate(deathParticle, player.transform.position, player.transform.rotation);
+        player.enabled = false;
+        player.GetComponent<Renderer>().enabled = false;
+        camera.isFollowing = false;
+        //gravityStore = player.GetComponent<Rigidbody2D> ().gravityScale;
+        //player.GetComponent<Rigidbody2D> ().gravityScale = 0f;
+        //player.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+        Debug.Log("Player Respawn");
+        yield return new WaitForSeconds(respawnDelay);
+        //player.GetComponent<Rigidbody2D> ().gravityScale = gravityStore;
+
+        // COMMENT/UNCOMMENT THIS AFTER TESTING
+        if (!loadedIn)
+        {
+            LoadLevel();
+        }
+
+        player.transform.position = currentCheckpoint.transform.position;
+
+        player.knockbackCount = 0;
+        player.enabled = true;
+        player.GetComponent<Renderer>().enabled = true;
+        HealthManager.FullHealth();
+        healthManager.isDead = false;
+        camera.isFollowing = true;
+        Instantiate(respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
+    }
+
+    public virtual void LoadLevel()
+    {
+
+    }
+}
--- System Dot v.06/Assets/Scripts/LevelManager.cs
+++ System Dot v.06/Assets/Scripts/LevelManager.cs
@@ -1,24 +1,7 @@
 ﻿ using UnityEngine;
 using System.Collections;
 
-public class LevelManager : MonoBehaviour {
-
-	public GameObject currentCheckpoint;
-
-	private PlayerController player;
-
-	public GameObject deathParticle;
-	public GameObject respawnParticle;
-
-	public int pointsPenaltyOnDeath;
-
-	public float respawnDelay;
-
-	private CameraController camera;
-
-	private float gravityStore;
-
-	public HealthManager healthManager;
+public class LevelManager : LevelHandler {
 
     [Header("Level 1 Checkpoints")]
     public GameObject checkpoint1;
@@ -37,64 +20,10 @@ public class LevelManager : MonoBehaviour {
     public GameObject chestHelpButton;
     public GameObject DebugButton;
 
-    public static bool canPressTab = true;
-
-
-    private bool loadedIn = false;
-    // Use this for initialization
-    void Start () {
-		player = FindObjectOfType<PlayerController> ();
-
-		camera = FindObjectOfType<CameraController> ();
-
-		healthManager = FindObjectOfType<HealthManager> ();
-        LoadLevel();
-        player.transform.position = currentCheckpoint.transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	public void RespawnPlayer()
-	{
-		StartCoroutine ("RespawnPlayerCo");
-	}
-
-	public IEnumerator RespawnPlayerCo()
-	{
-		Instantiate (deathParticle, player.transform.position, player.transform.rotation);
-		player.enabled = false;
-		player.GetComponent<Renderer>().enabled = false;
-		camera.isFollowing = false;
-		//gravityStore = player.GetComponent<Rigidbody2D> ().gravityScale;
-		//player.GetComponent<Rigidbody2D> ().gravityScale = 0f;
-		//player.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
-		Debug.Log ("Player Respawn");
-		yield return new WaitForSeconds (respawnDelay);
-        //player.GetComponent<Rigidbody2D> ().gravityScale = gravityStore;
-
-        // COMMENT/UNCOMMENT THIS AFTER TESTING
-        if (!loadedIn) { 
-            LoadLevel();
-        }
-
-        player.transform.position = currentCheckpoint.transform.position;
-
-		player.knockbackCount = 0;
-		player.enabled = true;
-		player.GetComponent<Renderer>().enabled = true;
-		HealthManager.FullHealth ();
-		healthManager.isDead = false;
-		camera.isFollowing = true;
-		Instantiate (respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
-	}
-
-    public void LoadLevel()
+    public override void LoadLevel()
     {
         // set it to false so we don't have to go through the tutorial
-        loadedIn = true;
+        base.loadedIn = true;
         switch (PlayerStats.checkpoint)
         {
             case "Checkpoint1":
