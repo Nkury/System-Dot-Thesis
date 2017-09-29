9547f9f579233ce1ad5f7ff731e8c60cb5858481
Nizar Kury <nkury@asu.edu>
2017-09-29 00:20:36 -0700

    level 3 boss progress made

--- System Dot v.06/Assets/Scripts/Level3BossBehavior.cs
+++ System Dot v.06/Assets/Scripts/Level3BossBehavior.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,22 +8,28 @@ public class Level3BossBehavior : MonoBehaviour {
 
     public GameObject[] pipeExits = new GameObject[4];
     public GameObject bossAttack;
+    public GameObject numberBall;
     public ParticleSystem spawnNumberBall;
     public Slider bossHealth;
 
     public float moveSpeed;
     public float amplitude;
     public float baseX;
+    public float spawnYDown;
 
     public int health;
+
     public float intervalToSpike;
+    public float intervalToSpawn;
 
 
     private float time = 0;
+    private float spawnTime = 0;
 
     private float midpoint1;
     private float midpoint2;
     private float midpoint3;
+    private float chosenMidpoint;
 
     private bool isAttacking;
     private bool isSpawning;
@@ -36,7 +43,13 @@ public class Level3BossBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+      
         transform.position = new Vector2(baseX + amplitude * Mathf.Sin(moveSpeed * Time.time), transform.position.y);
+
+        if (isSpawning && this.transform.position.x <= closestDistance() + .1f && this.transform.position.x >= closestDistance() - .1f)
+        {
+            transform.position = new Vector2(closestDistance(), this.transform.position.y);
+        }
         
         if (health != this.GetComponent<EnemyHealthManager>().enemyHealth)
         {
@@ -54,16 +67,52 @@ public class Level3BossBehavior : MonoBehaviour {
     void FixedUpdate()
     {
         time += Time.deltaTime;
+        spawnTime += Time.deltaTime;
+
         if (time >= intervalToSpike)
         {
             Spike();
         }
-        
+
+        if (spawnTime >= intervalToSpawn)
+        {
+            isSpawning = true;
+            if (spawnTime >= intervalToSpawn + 3)
+            {
+                SpawnNumberBall();
+            }
+        }
+
+    }
+
+    public float closestDistance()
+    {
+        float[] values =
+        {
+            Mathf.Abs(this.transform.position.x - midpoint1),
+            Mathf.Abs(this.transform.position.x - midpoint2),
+            Mathf.Abs(this.transform.position.x - midpoint3)
+        };
+
+        float[] midpoints =
+        {
+            midpoint1,
+            midpoint2,
+            midpoint3
+        };
+
+        float min = Mathf.Min(values);
+        int indexOfMidpoint = Array.IndexOf(values, min);
+        chosenMidpoint = midpoints[indexOfMidpoint];
+        return midpoints[indexOfMidpoint];
     }
 
     public void SpawnNumberBall()
     {
-        isSpawning = true;
+        GameObject particleSys = Instantiate(spawnNumberBall.gameObject, this.transform.position, spawnNumberBall.gameObject.transform.rotation);
+        GameObject numBall = Instantiate(numberBall, this.transform.position - new Vector3(0, spawnYDown), numberBall.transform.rotation);
+        spawnTime = 0;
+        isSpawning = false;
     }
     
     public void Spike()
